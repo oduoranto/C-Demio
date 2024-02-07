2df18b92f5a5a9e1f9f946a7bb3db5421fd115cd
antony oduor <antonyoduor72@gmail.com>
2024-02-07 22:43:00 +0300

    First C++ commit

---
+++ anto/Project1.0/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+  int sum;
+  cout << "Enter a number: ";cin >> a;
+  cout << "Enter a second number: "; cin >> b;
+  sum = a + b;
+  cout << "The sum of two numbers = " << sum;
+}
